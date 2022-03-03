518556c65894e0cbe5eab5e0873cd9603738a873
i.sumin <i.sumin@tinkoff.ru>
2022-03-03 17:56:32 +0300

    Add project files.

---
+++ MyFirstServer/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MyFirstServer
+{
+    class Client
+    {
+        public Client(TcpClient Client)
+        {
+            DateTime timestamp = DateTime.Now;
+            string response;
+            string Str;
+            byte[] request = new byte[4046];
+            Client.GetStream().Read(request, 0, request.Length);
+
+            Console.WriteLine(Encoding.ASCII.GetString(request).ToString());
+
+            switch(Encoding.ASCII.GetString(request).ToString().Split(' ')[1])
+            {
+                case "/get_summary":
+                    response = "{\"Result\":\"Success\",\"Summ\":\"" + Server.GetValSum() + "\",\"Timestamp\":\""+ timestamp + "\"}";
+                    Str = "HTTP/1.1 200 OK\nContent-type: application/json\nContent-Length:" + response.Length.ToString() + "\n\n" + response;
+                    break;
+                case "/post_data":
+                    string jsonReq = Encoding.ASCII.GetString(request).Substring(Encoding.ASCII.GetString(request).ToString().IndexOf('{')).Split("\0")[0];
+                    Server.AddParams(jsonReq);
+                    response = "{\"Result\":\"Success\"}";
+                    Str = "HTTP/1.1 200 OK\nContent-type: application/json\nContent-Length:" + response.Length.ToString() + "\n\n" + response;
+                    break;
+                default:
+                    response = "{\"Result\":\"ERROR\",\"Message\":\"path not found\"}";
+                    Str = "HTTP/1.1 400 ERROR\nContent-type: application/json\nContent-Length:" + response.Length.ToString() + "\n\n" + response;
+                    break;
+            }
+            byte[] Buffer = Encoding.ASCII.GetBytes(Str);
+            Client.GetStream().Write(Buffer, 0, Buffer.Length);
+            Client.Close();
+        }
+    }
+
+}
+
---
+++ MyFirstServer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MyFirstServer
+{
+    class Server
+    {
+        TcpListener Listener;
+        static Stack<DB> LocalDB = new Stack<DB>();
+        static DB LocalDataBaseObj = new DB();
+        public Server(int Port)
+        {
+            Listener = new TcpListener(IPAddress.Any, Port);
+            Listener.Start();
+            while (true)
+            {
+                new Client(Listener.AcceptTcpClient());
+            }
+        }
+        static public void AddParams(string Data)
+        {
+            LocalDataBaseObj = JsonSerializer.Deserialize<DB>(Data);
+            LocalDB.Push(LocalDataBaseObj);
+        }
+        static public int GetValSum()
+        {
+            int sum = 0;
+            foreach(DB obj in LocalDB)
+            {
+                sum += obj.Val;
+            }
+
+            return sum;
+        }
+        ~Server()
+        {
+            if(Listener != null)
+            {
+                Listener.Stop();
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Server(80);
+        }
+    }
+    class DB
+    {
+        public string Name { get; set; }
+        public int Val { get; set; }
+    }
+}
